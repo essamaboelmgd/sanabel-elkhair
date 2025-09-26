4a1d437ff7bc0ad540edfc29dec5602e945ae3b5
essamaboelmgd1 <essamaboelmgd1@gmail.com>
2025-09-26 22:59:45 +0300

    update cors origin

--- market-backend-api/app/main.py
+++ market-backend-api/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:3002", "http://localhost:3000","https://www.sanabelkhair.com","https://sanabelkhair.com"],
+    # allow_origins=["http://localhost:3001", "http://localhost:3000","https://www.sanabelkhair.com","https://sanabelkhair.com"],
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
